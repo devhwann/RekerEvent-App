b9a7297de29249c3a6d239408e6cfefc13b8dfc0
hwan97 <hg_07@naver.com>
2020-01-12 20:58:53 +0900

    api 설정

--- reker-frontend/src/components/common/Header/Header.js
+++ reker-frontend/src/components/common/Header/Header.js
@@ -1,20 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import classNames from "classnames/bind";
+import classNames from 'classnames/bind';
 import styles from './Header.scss';
 import Modal from 'react-bootstrap/Modal';
 import { Button } from 'react-bootstrap';
-
-
 
 const cx = classNames.bind(styles);
 
 const textMap = {
   register: '회원가입',
-  login: '로그인'
-}
+  login: '로그인',
+};
 
 const Header = (type, onsubmit) => {
   const text = textMap[type];
@@ -25,38 +23,37 @@ const Header = (type, onsubmit) => {
 
   return (
     <div classNames="cx">
-    <Button variant="primary" onClick={handleShow}>
+      <Button variant="primary" onClick={handleShow}>
         로그인
       </Button>
-    <Button variant="primary" onClick={handleShow}>
+      <Button variant="primary" onClick={handleShow}>
         회원가입
       </Button>
-      <Modal show={show} onHide={handleClose}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
       >
         <Modal.Header closeButton>
           <Modal.Title>{text}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-
-          <form onsubmit={onsubmit}>
-          <input type="text" name="username" value="" />
-          <input type="text" name="password" value="" />
-          {type === 'register' && (
-            <input type="text" name="passwordConfirm" />
-          )}
-          </form>
-
+          {/* <form onsubmit={onsubmit}> */}
+          아이디<input type="text" name="username" value="" />
+          비밀번호<input type="text" name="password" value="" />
+          {type === 'register' && <input type="text" name="passwordConfirm" />}
+          {/* </form> */}
+          Link
         </Modal.Body>
         <Modal.Footer>
-        {type === 'login' ? (
-          <Link to="/register">회원가입</Link>
-        ) : (
-          <Link to="/login">로그인</Link>
-        )}
-          {/* <Button variant="secondary" onClick={handleClose}>
+          {/* {type === 'login' ? (
+            <Link to="/register">회원가입</Link>
+          ) : (
+            <Link to="/login">로그인</Link>
+          )}
+          <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
           <Button variant="primary" onClick={handleClose}>
