76c5e84479b3a7c3f5aea99cf6078ed8805939cc
Hwan <jhxxx7@gmail.com>
2020-01-25 16:43:29 +0900

    Rotuer

--- reker-frontend/src/App.js
+++ reker-frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import MainPage from './pages/MainPage';
 // import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <MainPage/>
+    <>
+      <Route component={MainPage} path="/" exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+    </>
   );
-}
-
+};
 export default App;
