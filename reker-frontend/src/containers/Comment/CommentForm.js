aa3324f01b78295edc744975c374024a2464f91e
Hwan <jhxxx7@gmail.com>
2020-05-18 02:23:28 +0900

    Update Comment Write fun

--- reker-frontend/src/containers/Comment/CommentForm.js
+++ reker-frontend/src/containers/Comment/CommentForm.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, write } from '../../modules/comment';
-// import AuthForm1 from '../../components/Auth1/AuthForm1';
 import CommentForm from '../../components/Comment/CommenForm';
+import { check } from '../../modules/user';
 import { withRouter } from 'react-router-dom';
 
 
 const CommenForm = ({ history }) => {
   const [error, setError] = useState(null);
   const dispatch = useDispatch();
-  const { form, comment } = useSelector(({ comment, user }) => ({
+  const { form, comment, commentError, user } = useSelector(({ comment, user }) => ({
     form: comment.write,
-    auth1: comment.comment,
+    comment: comment.comment,
+    user: user.user
     // auth1Error: auth1.auth1Error,
   }));
 
@@ -29,10 +30,52 @@ const CommenForm = ({ history }) => {
 
   // 폼 등록 이벤트 핸들러
   const onSubmit = e => {
+    e.preventDefault();
+    const {body} = form;    
+    if ([body].includes('')) {
+      setError('빈 칸을 입력하세요.');
+      return;
+    }
+
+    dispatch(write({ body }));
     
   };
 
   // 컴포넌트가 처음 렌더링 될 때 form 을 초기화함
+  useEffect(() => {
+    dispatch(initializeForm('comment'));
+  }, [dispatch]);
+
+
+    useEffect(() => {
+    if (commentError) {
+      // 계정명이 이미 존재할 때
+      
+      // 기타 이유
+      // setError('회원가입 실패');
+      console.log('댓글 실패');
+      return;
+    }
+
+    if (comment) {
+      console.log('댓글 성공');
+      console.log(comment);
+      dispatch(check());
+    }
+  }, [comment, commentError, dispatch]);
+
+  // user 값이 잘 설정되었는지 확인
+  useEffect(() => {
+    if (user) {
+      history.push('/'); // 홈 화면으로 이동
+      try {
+        localStorage.setItem('user', JSON.stringify(user));
+      } catch (e) {
+        console.log('localStorage is not working');
+      }
+    }
+  }, [history, user]);
+  
 
   // 회원가입 성공 / 실패 처리
 
